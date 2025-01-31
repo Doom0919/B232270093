16afc7c25ca465a3c5eb10078e2b837dabdba888
B232270093 <boldoobuyka25@gmail.com>
2025-01-31 15:58:03 +0800

    First command

---
+++ Java/Main.java
@@ -0,0 +1,9 @@
+public Main{
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int sum = sumArray(arr);
+        System.out.println("Sum of the array elements: " + sum);
+    }
+    
+}
