b9c5dcae0754038b3d65a8d86b9bdd4a33f73333
B232270093 <boldoobuyka25@gmail.com>
2025-01-31 16:00:06 +0800

    command

--- Java/Main.java
+++ Java/Main.java
@@ -1,9 +1,21 @@
-public Main{
+public class Main {
+
+    private static int sumArray(int[] arr) {
+        int sum = 0;
+       for (int i = 0; i < arr.length;i++){
+        sum += arr[i];  // Add the current element to the sum
+        }
+        return sum;
+       }
+    
+    
+
 
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
         int sum = sumArray(arr);
-        System.out.println("Sum of the array elements: " + sum);
-    }
-    
+                System.out.println("Sum of the array elements: " + sum);
+            }
+        
+
 }
